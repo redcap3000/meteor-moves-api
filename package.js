58566ff005212c70c597283125eb7c85fb74f89c
Ronaldo Barbachano <redcap3000@gmail.com>
2014-08-16 22:20:11 +0000

    removed gmaps stuff, added gendates functions to server, cleaned up code, removed templates, removed attempt to make mongodbs

--- package.js
+++ package.js
@@ -3,8 +3,5 @@ Package.describe({
 });
 
 Package.on_use(function(api){
-  api.use(["underscore","templating"],"client");
   api.add_files("server.js","server");
-  //api.add_files("both.js",["server","client"]);
-  api.add_files(["templates.html","client.js"],"client");
 });
